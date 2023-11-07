615f7700a6d3e8be79e33a452d43c0802563211e
KarinaKutikov <karina.kutikov@limgeomatics.com>
2023-11-07 09:26:41 -0500

    made the back_to_settings_btn work on activity_about.xml

--- app/src/main/java/com/example/myapplication/AboutActivity.java
+++ app/src/main/java/com/example/myapplication/AboutActivity.java
@@ -5,22 +5,36 @@ import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.widget.ConstraintLayout;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.ScrollView;
 import android.widget.TextView;
 
+import org.json.JSONObject;
+
 public class AboutActivity extends AppCompatActivity {
+    private JSONObject configValues = new JSONObject();
     protected ScrollView aboutScrollView;
     protected ConstraintLayout aboutBox;
     protected TextView about;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
+
+        ImageView backToSettingsBtn = findViewById(R.id.back_to_settings_btn);
+        backToSettingsBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.d("configurationTransfer", configValues.toString());
+                finish();
+            }
+        });
     }
+
 }
 
 
